8cd1a4ecd4f727794dc9d84ca21a9905a75e4060
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:24:28 +0700

    Add author information to the script

--- Bài 4/3.15.py
+++ Bài 4/3.15.py
@@ -1,5 +1,5 @@
 
-print("##################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 
 input_string = input("nhap chuoi so tu tieng anh:")
 #tach chuoi thanh danh sach cac tu
@@ -11,3 +11,4 @@ sorted_words = sorted(words)
 #in danh sach cac tu da sap xep
 for word in sorted_words:
     print(word)
+
