07360607e92f62749da17aec626f0a9818eec026
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:36:51 +0700

    Update 6.8.py

--- bai 6/6.8.py
+++ bai 6/6.8.py
@@ -1,5 +1,5 @@
 
-print("############################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 class Bank:
     Account_type = "Savings"
     location = "Guntur"
@@ -75,3 +75,4 @@ def Exit(self):
 
 t1 = Bank('mahseh', 1453210145, 5000)
 print(t1)
+
