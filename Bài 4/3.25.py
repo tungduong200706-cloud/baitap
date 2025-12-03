177fb20321109d9f8e2754a4516f8d34efc8dfe1
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:32:25 +0700

    Refactor code to include author information

--- Bài 4/3.25.py
+++ Bài 4/3.25.py
@@ -1,5 +1,6 @@
 
-print("################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 numbers = input("Nhập danh sách số, cách nhau bởi dấu phẩy: ").split(',')
 odd_numbers = [num for num in numbers if int(num) % 2 != 0]
 print(','.join(odd_numbers))
+
