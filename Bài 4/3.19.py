d098b816bd9393206bab3f5841a7835a8399f163
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:28:08 +0700

    Add author information and fix limit variable name

--- Bài 4/3.19.py
+++ Bài 4/3.19.py
@@ -1,5 +1,5 @@
 
-print("##################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 def is_prime(num):
     return num > 1 and all(num % i != 0 for i in range(2, int(num**0.5) +1))
 
@@ -7,3 +7,4 @@ Limit = 10000000
 prime_tuple = tuple(num for num in range(2, limit) if is_prime(num))
 print(f"so nguyen to nho hon {'Limit'}:")
 print(prime_tuple)
+
