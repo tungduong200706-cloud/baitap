d1d4ee5cc8cdea3595dd40b40b1172fc0c5dc186
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:31:16 +0700

    Add author information and format input prompt

--- Bài 4/3.24.py
+++ Bài 4/3.24.py
@@ -1,5 +1,5 @@
 
-print("################################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 sentence = input("Nhập một câu: ")
 
 uppercase_count = sum(c.isupper() for c in sentence)
@@ -7,3 +7,4 @@ lowercase_count = sum(c.islower() for c in sentence)
 
 print(f"Chữ hoa: {uppercase_count}")
 print(f"Chữ thường: {lowercase_count}")
+
