db585fd9f61656b34302142e51aeb78a084250e4
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:32:40 +0700

    Update 3.26.py

--- Bài 4/3.26.py
+++ Bài 4/3.26.py
@@ -1,5 +1,6 @@
 
-print("################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 transactions = input("Nhập nhật ký giao dịch, cách nhau bởi dấu phẩy: ").split(',')
 account_balance = sum(map(float, transactions))
 print(f"số tiền thực trong tài khoản: {account_balance}")
+
