d8324207ddec6ad8de73b43ca77e7b9cbc84608b
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:32:56 +0700

    Update 3.3.py

--- Bài 4/3.3.py
+++ Bài 4/3.3.py
@@ -1,8 +1,9 @@
 
-print("##################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 
 s = input('Nhap chuoi:')
 for ch in s:
     print(ch.upper(), end='')
     '''ch.upper() se chuyen moi ky tu ch thanh chu in hoa
     va end=''se ngan xuong dong sau moi ky tu in hoa.'''
+
