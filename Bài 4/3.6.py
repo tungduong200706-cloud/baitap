e1b3dd1216fc1f3dd57f9374411da9f171f3d9a3
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:33:46 +0700

    Update 3.6.py

--- Bài 4/3.6.py
+++ Bài 4/3.6.py
@@ -1,5 +1,5 @@
 
-print("##################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 
 # nhap ten nguoi tu ban phim
 ten_nguoi = input ("nhap ten nguoi:")
@@ -11,3 +11,4 @@ ten_rieng = ten_nguoi[1:]
 # In phan ho va ten rieng ra man hinh
 print("ho: "+ho)
 print("Ten rieng:"+ten_rieng)
+
