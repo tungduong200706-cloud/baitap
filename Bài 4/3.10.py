270e38fe8eddb3dc2b2aec24d6c43f917753536f
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:22:40 +0700

    Update 3.10.py

--- Bài 4/3.10.py
+++ Bài 4/3.10.py
@@ -1,7 +1,8 @@
 
-print("##################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 ds = input('nhap chuoi: ').split()
 x = ds[0:-1]
 for c in x:
     print(c)
     
+
