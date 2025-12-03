36b06ea68cc9c9c674eaf54d7baaaa1eda7d7615
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:28:32 +0700

    Add header information and clean up whitespace

--- Bài 4/3.2.py
+++ Bài 4/3.2.py
@@ -1,10 +1,11 @@
 
-print("##################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 
 s = input('Nhap chuoi:')
 for ch in s:
     if ch.strip(): #Bo qua dau space va tab
         print(ch.upper(), end='')
+
 
 
 
