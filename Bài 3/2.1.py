78ce1afbcdb0b2be17a716ded181ea63873e2a91
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:06:52 +0700

    Update 2.1.py

--- Bài 3/2.1.py
+++ Bài 3/2.1.py
@@ -1,9 +1,10 @@
 
-print("########################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 
 def sum(a, b):
  print("sum = " + str(a+b))
-# tinh tong 2 so 5,8
-sum(5, 8)
-# tinh tong 2 so 0,4
-sum(0, 4)
+# tinh tong 2 so 4,5
+sum(4, 5)
+# tinh tong 2 so 3,7
+sum(3, 7)
+
