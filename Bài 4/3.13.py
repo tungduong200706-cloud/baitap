fd5b0ca8bc6108032e806beec6be6026691e2d28
tungduong200706-cloud <tungduong200706@gmail.com>
2025-12-03 08:23:42 +0700

    Refactor code to include author information

--- Bài 4/3.13.py
+++ Bài 4/3.13.py
@@ -1,7 +1,8 @@
 
-print("##################################")
+print('ho ten: Dang Phan Tung Duong; MAV:245752021610119')
 #nhap vao list
 ds = input('nhap chuoi: ').split()
 
 #tim phan tu trong list
 print("vi tri cua chuoi abc la", ds.index('abc'))
+
